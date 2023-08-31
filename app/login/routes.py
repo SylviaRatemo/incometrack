285ff26f0da001ccafe9db6c0700c55b76b0eec8
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-08-31 22:07:33 +0300

    Added the dashboard page

--- app/login/routes.py
+++ app/login/routes.py
@@ -4,21 +4,22 @@ from app.models.login import checkCredentials
 #from app.houses import bp
 
 
-#@bp.route('/')
-@bp.route('/login', methods=['POST'])
+@bp.route('/')
+@bp.route('/login', methods=['POST', 'GET'])
 def login():
-	_json = request.json
-	#print(_json)
-	_email = _json['email']
-	_password = _json['password']
-	
-	if _email and _password:
-		user = checkCredentials(_email, _password)
-		if user != None:
-			session['username'] = user
-			print("Welcome, " + session.get('username'))
-			return jsonify(user)
-		return render_template('login.html')
+	if request.method == 'POST':
+		_json = request.json
+		#print(_json)
+		_email = _json['email']
+		_password = _json['password']
+		
+		if _email and _password:
+			user = checkCredentials(_email, _password)
+			if user != None:
+				session['username'] = user
+				print("Welcome, " + session.get('username'))
+				return jsonify(user)
+	return render_template('login.html')
 
 @bp.route('/logout')
 def logout():
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,8 @@
 from app.main import bp 
 from flask import render_template, jsonify, session, request
+from app.decorators import login_required
 
-@bp.route('/')
+@bp.route('/dashboard')
+@login_required
 def index():
 	return render_template('index.html')
